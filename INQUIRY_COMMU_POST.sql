3577592c2fb7f6db35de2804fd7ea52a9ffd87d0
morris <dltkdah777@gmail.com>
2025-01-16 19:10:54 +0900

    feat : 게시글 조회 sql

---
+++ INQUIRY_COMMU_POST.sql
@@ -0,0 +1,10 @@
+-- 게시글 조회 기능
+SELECT
+       community_post.post_id,
+       community_post.post_title,
+       community_post.post_created_at,
+       community_post.post_updated_at,
+       community_post.post_is_blinded,
+       community_post.user_id
+  FROM community_post
+ WHERE post_is_blinded = FALSE;
