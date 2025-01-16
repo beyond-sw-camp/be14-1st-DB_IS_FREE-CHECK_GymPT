7faaddcfb0f8a976c0f331d393fae2c56a6b465a
MJay1123 <tjalswhd1@naver.com>
2025-01-16 20:42:21 +0900

    운동 장소 리뷰 검색 기능 추가 등록시간 순 정렬 / 키워드 포함 검색

--- place_review_search.sql
+++ place_review_search.sql
@@ -1,5 +1,4 @@
--- 운동 장소 리뷰 검색
-
+-- 운동 장소 리뷰 조회
 SELECT
 		 review_title,
 		 review_content,
@@ -11,3 +10,16 @@ SELECT
 		 user_id
   FROM place_review
  WHERE review_title LIKE '%별로%';
+ 
+-- 운동 장소 리뷰 조회
+SELECT
+		 review_title,
+		 review_content,
+		 review_rating,
+		 review_created_at,
+		 review_updated_at,
+		 review_is_blinded,
+		 wplace_id,
+		 user_id
+  FROM place_review
+  ORDER BY review_created_at DESC;
