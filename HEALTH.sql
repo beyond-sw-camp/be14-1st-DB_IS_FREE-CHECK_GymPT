22a25307180818c121eb7d1aec57054289bffe69
Bluesky3125 <charlie4822@gmail.com>
2025-01-16 16:42:38 +0900

    unique, 이메일 등 유효성 검사, default 값, comment 등 추가

--- HEALTH.sql
+++ HEALTH.sql
@@ -20,23 +20,25 @@ DROP TABLE IF EXISTS user CASCADE;
 
 CREATE TABLE IF NOT EXISTS user
 (
-    user_id       INT           NOT NULL AUTO_INCREMENT,
-    user_email    VARCHAR(50)   NOT NULL,
-    user_pw       VARCHAR(20)   NOT NULL,
-    user_phone    VARCHAR(20)   NOT NULL,
-    user_name     VARCHAR(20)   NOT NULL,
-    user_gender   CHAR(1)       NOT NULL,
-    user_address  VARCHAR(20)   NOT NULL,
-    user_image    VARCHAR(1000) NOT NULL,
-    user_nickname VARCHAR(20)   NOT NULL,
-    user_height   DECIMAL(5, 2) NOT NULL,
-    user_weight   DECIMAL(5, 2) NOT NULL,
-    user_join     DATE          NOT NULL,
-    user_leave    DATE          NULL,
-    user_report   TINYINT       NOT NULL DEFAULT 0,
-    user_grant    CHAR(1)       NOT NULL,
+    user_id       INT           NOT NULL AUTO_INCREMENT COMMENT '회원 ID',
+    user_email    VARCHAR(50)   NOT NULL UNIQUE COMMENT '회원 이메일' CHECK (user_email LIKE '%@%'),
+    user_pw       VARCHAR(20)   NOT NULL COMMENT '회원 비밀번호',
+    user_phone    VARCHAR(20)   NOT NULL UNIQUE COMMENT '회원 연락처',
+    user_name     VARCHAR(20)   NOT NULL COMMENT '회원 이름',
+    user_gender   CHAR(1)       NOT NULL COMMENT '회원 성별' CHECK (user_gender IN ('M', 'F')),
+    user_address  VARCHAR(20)   NOT NULL COMMENT '회원 주소',
+    user_image    VARCHAR(1000) NOT NULL COMMENT '회원 프로필 사진 URL',
+    user_nickname VARCHAR(20)   NOT NULL COMMENT '회원 별명',
+    user_height   DECIMAL(5, 2) NOT NULL COMMENT '회원 키',
+    user_weight   DECIMAL(5, 2) NOT NULL COMMENT '회원 몸무게',
+    user_join     DATE          NOT NULL DEFAULT (CURRENT_DATE) COMMENT '가입일자',
+    user_leave    DATE          NULL COMMENT '탈퇴일자',
+    user_report   TINYINT       NOT NULL DEFAULT 0 COMMENT '신고받은 횟수',
+    user_grant    CHAR(1)       NOT NULL DEFAULT 'N' COMMENT '관리자 권한' CHECK (user_grant IN ('Y', 'N')),
     PRIMARY KEY (user_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='회원'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS sticker
 (
@@ -44,12 +46,14 @@ CREATE TABLE IF NOT EXISTS sticker
     sticker_size  VARCHAR(255) NOT NULL,
     sticker_color VARCHAR(255) NOT NULL,
     PRIMARY KEY (sticker_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='다이어리 스티커'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS diary
 (
     diary_id     INT          NOT NULL AUTO_INCREMENT,
-    date         DATE         NOT NULL,
+    date         DATE         NOT NULL DEFAULT (CURRENT_DATE),
     title        VARCHAR(255) NOT NULL,
     task         VARCHAR(255) NOT NULL,
     is_completed VARCHAR(255) NOT NULL,
@@ -59,55 +63,62 @@ CREATE TABLE IF NOT EXISTS diary
     PRIMARY KEY (diary_id),
     FOREIGN KEY (user_id) REFERENCES user (user_id),
     FOREIGN KEY (sticker_id) REFERENCES sticker (sticker_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='다이어리'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS wPlace
 (
     wplace_id      INT          NOT NULL AUTO_INCREMENT,
-    wplace_name    VARCHAR(100) NOT NULL,
-    wpalce_adress  VARCHAR(255) NOT NULL,
-    wplace_des     TEXT(3000)   NOT NULL,
-    wcreate_at     DATETIME     NOT NULL,
-    wupdate_at     DATETIME     NULL,
-    wplace_request DATETIME     NOT NULL,
+    wplace_name    VARCHAR(100) NOT NULL UNIQUE,
+    wplace_address VARCHAR(255) NOT NULL UNIQUE,
+    wplace_des     TEXT(3000)   NOT NULL COMMENT '장소 설명',
+    wcreate_at     DATE         NOT NULL DEFAULT (CURRENT_DATE) COMMENT '장소 등록 일자',
+    wupdate_at     DATE         NULL COMMENT '장소 수정 일자',
     user_id        INT          NOT NULL,
     PRIMARY KEY (wplace_id),
     FOREIGN KEY (user_id) REFERENCES user (user_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='운동 장소'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS place_review
 (
     review_id         INT          NOT NULL AUTO_INCREMENT COMMENT '리뷰 ID',
-    review_title      VARCHAR(100) NOT NULL COMMENT '- 리뷰 제목',
-    review_content    TEXT(3000)   NOT NULL COMMENT '- 리뷰 내용',
-    review_rating     INT          NOT NULL COMMENT '- 리뷰 별점 (1~5 사이의 값)',
-    review_created_at DATETIME     NOT NULL COMMENT '- 리뷰 작성 시간',
-    review_updated_at DATETIME     NULL COMMENT '- 리뷰 수정 시간',
-    review_is_blinded BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '- 리뷰 수정 시간',
+    review_title      VARCHAR(100) NOT NULL COMMENT '리뷰 제목',
+    review_content    TEXT(3000)   NOT NULL COMMENT '리뷰 내용',
+    review_rating     INT          NOT NULL COMMENT '리뷰 별점 (1~5 사이의 값)',
+    review_created_at DATETIME     NOT NULL DEFAULT (CURRENT_TIME) COMMENT '리뷰 작성 시간',
+    review_updated_at DATETIME     NULL COMMENT '리뷰 수정 시간',
+    review_is_blinded BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '리뷰 수정 시간',
     wplace_id         INT          NOT NULL,
     user_id           INT          NOT NULL,
     PRIMARY KEY (review_id),
     FOREIGN KEY (wplace_id) REFERENCES wPlace (wplace_id),
     FOREIGN KEY (user_id) REFERENCES user (user_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='운동장소 리뷰'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS wInfo
 (
     winfo_id        INT          NOT NULL AUTO_INCREMENT,
-    winfo_name      VARCHAR(100) NOT NULL,
-    winfo_rule      TEXT(9000)   NOT NULL,
-    winfo_intensity INT          NOT NULL,
+    winfo_name      VARCHAR(100) NOT NULL COMMENT '운동 이름',
+    winfo_rule      TEXT(9000)   NOT NULL COMMENT '운동 방법',
+    winfo_intensity INT          NOT NULL COMMENT '운동 강도',
     PRIMARY KEY (winfo_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='운동 정보'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS wRecord
 (
     wrecord_id        INT  NOT NULL AUTO_INCREMENT,
-    wrecord_date      DATE NOT NULL,
-    wrecord_min       TIME NOT NULL,
-    wrecord_cal       INT  NOT NULL,
+    wrecord_date      DATE NOT NULL DEFAULT (CURRENT_DATE),
+    wrecord_min       TIME NOT NULL COMMENT '운동 시간(분)',
+    wrecord_cal       INT  NOT NULL COMMENT '소모 칼로리',
     average_pace      TIME NOT NULL,
-    wrecord_at        TIME NOT NULL,
+    wrecord_at        TIME NOT NULL COMMENT '기록 시간',
     wrecord_intensity INT  NOT NULL,
     user_id           INT  NOT NULL,
     workout_id        INT  NOT NULL,
@@ -116,18 +127,22 @@ CREATE TABLE IF NOT EXISTS wRecord
     FOREIGN KEY (user_id) REFERENCES user (user_id),
     FOREIGN KEY (workout_id) REFERENCES wInfo (winfo_id),
     FOREIGN KEY (wplace_id) REFERENCES wPlace (wplace_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='운동 기록'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS product
 (
     product_id         INT          NOT NULL AUTO_INCREMENT,
     product_name       VARCHAR(100) NOT NULL,
-    product_desc       TEXT(3000)   NOT NULL,
+    product_desc       TEXT(3000)   NOT NULL COMMENT '상품 설명',
     product_url        VARCHAR(255) NOT NULL,
-    product_created_at DATETIME     NOT NULL,
-    product_updated_at DATETIME     NULL,
+    product_created_at DATE         NOT NULL DEFAULT (CURRENT_DATE),
+    product_updated_at DATE         NULL,
     PRIMARY KEY (product_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='상품'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS workout_prod
 (
@@ -135,34 +150,40 @@ CREATE TABLE IF NOT EXISTS workout_prod
     product_id INT NOT NULL,
     FOREIGN KEY (workout_id) REFERENCES wInfo (winfo_id),
     FOREIGN KEY (product_id) REFERENCES product (product_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='운동 별 상품'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS studio
 (
     studio_id      INT            NOT NULL AUTO_INCREMENT,
     studio_name    VARCHAR(255)   NOT NULL,
-    studio_address TEXT(1000)     NOT NULL,
-    studio_des     TEXT(9000)     NOT NULL,
+    studio_address TEXT(1000)     NOT NULL UNIQUE,
+    studio_des     TEXT(9000)     NOT NULL COMMENT '스튜디오 설명',
     studio_price   DECIMAL(10, 2) NOT NULL,
-    created_at     DATETIME       NOT NULL,
-    updated_at     DATETIME       NULL,
+    created_at     DATE           NOT NULL DEFAULT (CURRENT_DATE),
+    updated_at     DATE           NULL,
     PRIMARY KEY (studio_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='스튜디오'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS studio_review
 (
     review_id         INT        NOT NULL AUTO_INCREMENT,
     review_content    TEXT(9000) NOT NULL,
-    review_rating     INT        NOT NULL,
-    review_created_at DATETIME   NOT NULL,
-    review_updated_at DATETIME   NULL,
-    review_is_blinded BOOLEAN    NOT NULL,
+    review_rating     INT        NOT NULL CHECK (review_rating BETWEEN 1 AND 5),
+    review_created_at DATE       NOT NULL DEFAULT (CURRENT_DATE),
+    review_updated_at DATE       NULL,
+    review_is_blinded BOOLEAN    NOT NULL DEFAULT FALSE,
     user_id           INT        NOT NULL,
     studio_id         INT        NOT NULL,
     PRIMARY KEY (review_id),
     FOREIGN KEY (user_id) REFERENCES user (user_id),
     FOREIGN KEY (studio_id) REFERENCES studio (studio_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='스튜디오 리뷰'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS studio_booking
 (
@@ -175,15 +196,19 @@ CREATE TABLE IF NOT EXISTS studio_booking
     PRIMARY KEY (booking_id),
     FOREIGN KEY (user_id) REFERENCES user (user_id),
     FOREIGN KEY (studio_id) REFERENCES studio (studio_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='스튜디오 예약'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS blacklist_level
 (
     level   INT         NOT NULL AUTO_INCREMENT,
     auth    VARCHAR(20) NOT NULL,
-    periods DATETIME    NOT NULL,
+    periods DATE        NOT NULL,
     PRIMARY KEY (level)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='블랙리스트 레벨'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS blacklist
 (
@@ -194,20 +219,24 @@ CREATE TABLE IF NOT EXISTS blacklist
     level      INT     NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (user_id),
     FOREIGN KEY (level) REFERENCES blacklist_level (level)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='블랙리스트'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS community_post
 (
     post_id         INT           NOT NULL AUTO_INCREMENT,
     post_title      VARCHAR(100)  NOT NULL,
     post_content    VARCHAR(6000) NOT NULL,
-    post_created_at DATETIME      NOT NULL,
+    post_created_at DATETIME      NOT NULL DEFAULT (CURRENT_TIME),
     post_updated_at DATETIME      NULL,
     post_is_blinded BOOLEAN       NOT NULL,
     user_id         INT           NOT NULL,
     PRIMARY KEY (post_id),
     FOREIGN KEY (user_id) REFERENCES user (user_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='커뮤니티 게시글'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS post_bookmark
 (
@@ -215,7 +244,9 @@ CREATE TABLE IF NOT EXISTS post_bookmark
     post_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (user_id),
     FOREIGN KEY (post_id) REFERENCES community_post (post_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='게시글 북마크'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS post_like
 (
@@ -223,16 +254,17 @@ CREATE TABLE IF NOT EXISTS post_like
     user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (user_id),
     FOREIGN KEY (post_id) REFERENCES community_post (post_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='게시글 좋아요'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS comment
 (
     comment_id         INT          NOT NULL AUTO_INCREMENT,
     comment_title      VARCHAR(300) NOT NULL,
-    comment_create_at  DATETIME     NOT NULL,
+    comment_create_at  DATETIME     NOT NULL DEFAULT (CURRENT_TIME),
     comment_delete_at  DATETIME     NULL,
-    user_nickname      VARCHAR(50)  NOT NULL,
-    comment_is_blinded BOOLEAN      NOT NULL,
+    comment_is_blinded BOOLEAN      NOT NULL DEFAULT FALSE,
     user_id            INT          NOT NULL,
     post_id            INT          NOT NULL,
     comment_id2        INT          NOT NULL,
@@ -240,16 +272,18 @@ CREATE TABLE IF NOT EXISTS comment
     FOREIGN KEY (user_id) REFERENCES user (user_id),
     FOREIGN KEY (post_id) REFERENCES community_post (post_id),
     FOREIGN KEY (comment_id2) REFERENCES comment (comment_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='댓글'
+  DEFAULT CHARSET UTF8;
 
 CREATE TABLE IF NOT EXISTS report
 (
     report_id       INT           NOT NULL AUTO_INCREMENT,
     user_id         INT           NOT NULL,
     report_reason   VARCHAR(9000) NOT NULL,
-    report_YN       BOOLEAN       NOT NULL,
-    reported_id     INT           NOT NULL,
-    report_category INT           NOT NULL,
+    report_YN       BOOLEAN       NOT NULL DEFAULT FALSE COMMENT '신고 처리 여부',
+    reported_id     INT           NOT NULL COMMENT '신고당한 회원 ID',
+    report_category INT           NOT NULL CHECK (report_category IN (1, 2, 3, 4)),
     post_id         INT           NULL,
     comment_id      INT           NULL,
     review_id       INT           NULL COMMENT '운동장소 리뷰 ID',
@@ -259,4 +293,6 @@ CREATE TABLE IF NOT EXISTS report
     FOREIGN KEY (comment_id) REFERENCES comment (comment_id),
     FOREIGN KEY (review_id) REFERENCES place_review (review_id),
     FOREIGN KEY (review_id2) REFERENCES studio_review (review_id)
-);
+) ENGINE = INNODB
+  AUTO_INCREMENT = 1 COMMENT ='신고 내역'
+  DEFAULT CHARSET UTF8;
