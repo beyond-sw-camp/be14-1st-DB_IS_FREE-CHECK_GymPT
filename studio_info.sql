a110359401efd7a1fe5f84e516c5a1c627753270
Gombo2 <rhtjddus0502@gmail.com>
2025-01-17 10:06:00 +0900

    스튜디오 정보 정렬 기준 재정의
    등록시간기준 -> 이름순으로 정렬

--- studio_info.sql
+++ studio_info.sql
@@ -9,8 +9,8 @@ SELECT studio_name,
            END AS TIME
 -- 등록시간 수정시간에 따라 TIME이라는 이름으로 수정되면 수정시간, 없으면 등록시간 출력
 FROM studio
-ORDER BY created_at DESC;
--- 등록 시간 기준 최신순으로 조회시켜줌.
+ORDER BY studio_name;
+-- 스튜디오 이름 기준으로 조회시켜줌.
 
 -- 사용자 키워드 입력에 따른 조회 기능
 SELECT studio_name,
@@ -23,5 +23,5 @@ SELECT studio_name,
            END AS TIME
 FROM studio
 WHERE studio_name LIKE '%스튜%'
-ORDER BY created_at DESC;
+ORDER BY studio_name;
 -- 키워드 입력 조건
