2f436fbc81c0c1f2c7b1aa2b8ca49acab463a4a9
morris <dltkdah777@gmail.com>
2025-01-17 11:17:41 +0900

    fix : 생성날짜와 업데이트 날짜 데이터 select의 선택적 처리

--- INQUIRY_COMMU_POST.sql
+++ INQUIRY_COMMU_POST.sql
@@ -1,12 +1,11 @@
 -- 게시글 조회 기능
-SELECT community_post.post_id
-     , community_post.post_title
-     , community_post.post_created_at
-     , community_post.post_updated_at
+SELECT post_id
+     , post_title
+     , COALESCE(post_updated_at, post_created_at) AS post_effective_date
      , u.user_id
      , u.user_nickname
      , u.user_image
 FROM community_post
-         JOIN user u ON u.user_id
+         JOIN user u ON u.user_id = post_id
 WHERE post_is_blinded = FALSE
 ORDER BY post_created_at ASC;
