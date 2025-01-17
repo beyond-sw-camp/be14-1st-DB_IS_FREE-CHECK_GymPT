883feb87874d1ecb7bfd272465e584ad90a0f561
MJay1123 <tjalswhd1@naver.com>
2025-01-17 09:43:29 +0900

    리뷰 검색 기능 블라인드 여부 적용 + 기본 내림차순 정리

--- place_review_search.sql
+++ place_review_search.sql
@@ -3,23 +3,23 @@ SELECT
 		 review_title,
 		 review_content,
 		 review_rating,
-		 review_created_at,
-		 review_updated_at,
-		 review_is_blinded,
-		 wplace_id,
-		 user_id
+		 case
+		 when review_updated_at IS NULL then review_created_at
+		 ELSE review_updated_at
+		 END AS time
   FROM place_review
- WHERE review_title LIKE '%별로%';
+ WHERE review_title LIKE '%별로%'
+   AND review_is_blinded = false;
  
 -- 운동 장소 리뷰 조회
 SELECT
 		 review_title,
 		 review_content,
 		 review_rating,
-		 review_created_at,
-		 review_updated_at,
-		 review_is_blinded,
-		 wplace_id,
-		 user_id
+		 case
+		 when review_updated_at IS NULL then review_created_at
+		 ELSE review_updated_at
+		 END AS TIME
   FROM place_review
+  WHERE review_is_blinded = FALSE
   ORDER BY review_created_at DESC;
