828f0a484aa28b35ba99be5bd55f381a1c4891d4
Hancoal <hshgood035@naver.com>
2025-01-17 14:03:27 +0900

    아직 수정중

--- wPLACE.sql
+++ wPLACE.sql
@@ -1,9 +1,14 @@
 -- 테스트 케이스시 사용할 관리자 권한을 가진 유저는 user_id 5(한석현)
 -- 운동장소 등록 /관리자만
-INSERT INTO wPlace (wplace_name, wplace_address, wplace_des, user_id)
-SELECT '동작 헬스장1', '서울특별시 보라매롱', '어딘가', 1
-FROM user
-WHERE user_id = 5 AND user_grant = 'Y';
+-- 관리자 권한 체크
+IF ((SELECT user_grant
+     FROM user
+     WHERE user_id = 5) = 'Y') THEN
+    INSERT INTO wplace (wplace_name, wplace_address, wplace_des, wcreate_at, wupdate_at, user_id)
+    VALUES ('인천2헬스장', '인천 어딘가2', '원숭이가많음', DEFAULT, NULL, 4);
+    else
+    select '관리자가 아닙니다';
+    END IF;
 
 -- 운동 장소 정보 수정 /관리자만
 UPDATE wPlace
@@ -19,7 +24,7 @@ UPDATE wPlace
 
 -- 운동장소 삭제 / 관리자만
 DELETE FROM wPlace
-                  WHERE wplace_id = 3
+                  WHERE wplace_id = 4
         AND EXISTS (
     SELECT 1
     FROM user
