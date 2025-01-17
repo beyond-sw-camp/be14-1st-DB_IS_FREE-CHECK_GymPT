f2461e52e0de4a16dd0d97ed9552b3cde6794af3
Bluesky3125 <charlie4822@gmail.com>
2025-01-17 12:51:09 +0900

    관리자 권한 체크 개선

--- studio_review_CUD.sql
+++ studio_review_CUD.sql
@@ -28,12 +28,17 @@ WHERE review_id = 3;
 
 -- 신고 처리에 따른 관리자의 스튜디오 리뷰 삭제
 SET FOREIGN_KEY_CHECKS = 0; -- 외래키 참조 해제
-DELETE
-FROM studio_review
-WHERE review_id IN (SELECT review_id2 -- 신고 처리된 글 선택
-                    FROM report
-                    WHERE report_YN = 1)
-  AND EXISTS(SELECT 1 -- 관리자인지 확인
-             FROM user
-             WHERE user_grant = 'Y');
+
+IF ((SELECT user_grant -- 관리자 권한 체크
+     FROM user
+     WHERE user_id = 4) = 'Y') THEN
+    DELETE
+    FROM studio_review
+    WHERE review_id IN (SELECT review_id2 -- 신고 처리된 글 선택
+                        FROM report
+                        WHERE report_YN = 1);
+ELSE
+    SELECT '관리자가 아닙니다';
+END IF;
+
 SET FOREIGN_KEY_CHECKS = 1; -- 외래키 참조 설정 복원
