4dac80f5832e7a97ec44faa1f964c46dc4f8c713
morris <dltkdah777@gmail.com>
2025-01-17 20:17:49 +0900

    fix : 중복된 create comment table 구문 삭제

--- create_tables.sql
+++ create_tables.sql
@@ -238,25 +238,6 @@ CREATE TABLE IF NOT EXISTS community_post
   AUTO_INCREMENT = 1 COMMENT ='커뮤니티 게시글'
   DEFAULT CHARSET UTF8;
 
--- SELECT * FROM comment;
-CREATE TABLE IF NOT EXISTS comment
-(
-    comment_id         INT          NOT NULL AUTO_INCREMENT,
-    comment_title      VARCHAR(300) NOT NULL,
-    comment_created_at DATETIME     NOT NULL DEFAULT (CURRENT_TIME),
-    comment_updated_at DATETIME     NULL,
-    comment_is_blinded BOOLEAN      NOT NULL DEFAULT FALSE,
-    user_id            INT          NOT NULL,
-    post_id            INT          NOT NULL,
-    comment_id2        INT          NULL,
-    PRIMARY KEY (comment_id),
-    FOREIGN KEY (user_id) REFERENCES user (user_id),
-    FOREIGN KEY (post_id) REFERENCES community_post (post_id),
-    FOREIGN KEY (comment_id2) REFERENCES comment (comment_id)
-) ENGINE = INNODB
-  AUTO_INCREMENT = 1 COMMENT ='댓글'
-  DEFAULT CHARSET UTF8;
-
 CREATE TABLE IF NOT EXISTS post_bookmark
 (
     user_id INT NOT NULL,
