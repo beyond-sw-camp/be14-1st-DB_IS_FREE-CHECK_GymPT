39a8f34cd584c5002752fe68278e2e9be31f6908
morris <dltkdah777@gmail.com>
2025-01-17 11:30:03 +0900

    fix : 잘못걸린 join 수정

--- INQUIRY_COMMU_POST.sql
+++ INQUIRY_COMMU_POST.sql
@@ -1,11 +1,11 @@
 -- 게시글 조회 기능
-SELECT post_id
-     , post_title
+SELECT p.post_id
+     , p.post_title
      , COALESCE(post_updated_at, post_created_at) AS post_effective_date
      , u.user_id
      , u.user_nickname
      , u.user_image
-FROM community_post
-         JOIN user u ON u.user_id = post_id
+FROM community_post p
+         JOIN user u ON u.user_id = p.user_id
 WHERE post_is_blinded = FALSE
 ORDER BY post_created_at ASC;
