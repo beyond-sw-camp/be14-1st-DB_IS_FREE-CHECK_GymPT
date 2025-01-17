fae3115408a6f389e33d7624535348411fb78745
Gombo2 <rhtjddus0502@gmail.com>
2025-01-17 09:38:20 +0900

    사용자 스튜디오 정보 조회 및 키워드 기능
    CASE를 이용하여 등록시간 수정시간중 하나만 출력
    WHERE 조건문 통하여 키워드 검색 기능 추가

--- studio_info.sql
+++ studio_info.sql
@@ -9,7 +9,8 @@ SELECT studio_name,
            END AS TIME
 -- 등록시간 수정시간에 따라 TIME이라는 이름으로 수정되면 수정시간, 없으면 등록시간 출력
 FROM studio
-WHERE studio_id = 3;
+ORDER BY created_at DESC;
+-- 등록 시간 기준 최신순으로 조회시켜줌.
 
 -- 사용자 키워드 입력에 따른 조회 기능
 SELECT studio_name,
@@ -21,5 +22,6 @@ SELECT studio_name,
            ELSE updated_at
            END AS TIME
 FROM studio
-WHERE studio_name LIKE '%포마%';
+WHERE studio_name LIKE '%스튜%'
+ORDER BY created_at DESC;
 -- 키워드 입력 조건
