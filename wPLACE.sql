db1f47b97c387ef183232923afaaf63b67e5b2cd
Hancoal <hshgood035@naver.com>
2025-01-17 14:13:09 +0900

    feat : 관리자 운동장소( wPLACE) 등록,수정,삭제 기능

--- wPLACE.sql
+++ wPLACE.sql
@@ -1,6 +1,5 @@
--- 테스트 케이스시 사용할 관리자 권한을 가진 유저는 user_id 5(한석현)
--- 운동장소 등록 /관리자만
--- 관리자 권한 체크
+-- 관리자 : user_id => 5
+-- 관리자 권한 확인 후 운동장소 (등록)
 IF ((SELECT user_grant
      FROM user
      WHERE user_id = 5) = 'Y') THEN
@@ -10,23 +9,27 @@ IF ((SELECT user_grant
     select '관리자가 아닙니다';
     END IF;
 
--- 운동 장소 정보 수정 /관리자만
-UPDATE wPlace
-    SET wplace_name = '동작 국립 헬스장',
-              wplace_address = '서울특별시 보라매로 87',
-              wplace_des = '기구가 친절하고 관장님이 좋네요'
-       WHERE wplace_id = 3
-         AND EXISTS (
-           SELECT 1
-           FROM user
-           WHERE user_id = 5 AND user_grant = 'Y'
-       );
-
--- 운동장소 삭제 / 관리자만
-DELETE FROM wPlace
-                  WHERE wplace_id = 4
-        AND EXISTS (
-    SELECT 1
+-- 관리자 권한 확인 후 운동장소 (수정)
+IF (SELECT user_grant
     FROM user
-    WHERE user_id = 5 AND user_grant = 'Y'
-);
+    WHERE user_id = 5) = 'Y' THEN
+-- 관리자 권한이 있을 경우 수정
+UPDATE wPlace
+SET wplace_name = '동작 국립 헬스장',
+    wplace_address = '서울특별시 보라매로 87',
+    wplace_des = '기구가 친절하고 관장님이 좋네요'
+WHERE wplace_id = 2;
+ELSE
+SELECT '관리자가 아니므로 수정이 불가합니다';
+END IF;
+
+-- 관리자 권한 확인 후 삭제
+IF (SELECT user_grant
+    FROM user
+    WHERE user_id = 5) = 'Y' THEN
+-- 관리자 권한이 있을 경우 삭제
+DELETE FROM wPlace
+WHERE wplace_id = 12;
+ELSE
+SELECT '관리자가 아니므로 삭제가 불가합니다';
+END IF;
