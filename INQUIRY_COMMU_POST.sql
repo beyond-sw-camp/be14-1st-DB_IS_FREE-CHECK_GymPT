fbc199c678f93e3a81d1c3ee195d5c10ad26c9a5
morris <dltkdah777@gmail.com>
2025-01-17 11:40:22 +0900

    fix : 게시물의 내용 select, 정렬 수정

--- INQUIRY_COMMU_POST.sql
+++ INQUIRY_COMMU_POST.sql
@@ -1,6 +1,7 @@
 -- 게시글 조회 기능
 SELECT p.post_id
      , p.post_title
+     , p.post_content
      , COALESCE(post_updated_at, post_created_at) AS post_effective_date
      , u.user_id
      , u.user_nickname
@@ -8,4 +9,4 @@ SELECT p.post_id
 FROM community_post p
          JOIN user u ON u.user_id = p.user_id
 WHERE post_is_blinded = FALSE
-ORDER BY post_created_at ASC;
+ORDER BY post_created_at DESC;
