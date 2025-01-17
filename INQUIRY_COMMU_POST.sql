88c0fcfa2fd595ef13862603ee1207abb815a3bf
morris <dltkdah777@gmail.com>
2025-01-17 10:47:31 +0900

    fix : 유저 정보는 join으로 user 테이블에서 select, 작성시간 기준 오름차순 정렬

--- INQUIRY_COMMU_POST.sql
+++ INQUIRY_COMMU_POST.sql
@@ -1,9 +1,12 @@
 -- 게시글 조회 기능
-SELECT
-       community_post.post_id,
-       community_post.post_title,
-       community_post.post_created_at,
-       community_post.post_updated_at,
-       community_post.user_id
-  FROM community_post
- WHERE post_is_blinded = FALSE;
+SELECT community_post.post_id
+     , community_post.post_title
+     , community_post.post_created_at
+     , community_post.post_updated_at
+     , u.user_id
+     , u.user_nickname
+     , u.user_image
+FROM community_post
+         JOIN user u ON u.user_id
+WHERE post_is_blinded = FALSE
+ORDER BY post_created_at ASC;
