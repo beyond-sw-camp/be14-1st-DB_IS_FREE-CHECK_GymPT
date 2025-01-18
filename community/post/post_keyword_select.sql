3c4e8496aa4d99b8a0d9d1b6b0d23831ac17c30c
morris <dltkdah777@gmail.com>
2025-01-18 15:10:38 +0900

    feat : 게시물의 내용, 제목 둘 다 검색하는 sql문 추가

--- community/post/post_keyword_select.sql
+++ community/post/post_keyword_select.sql
@@ -27,3 +27,20 @@ WHERE post_is_blinded = 0
   AND c.comment_is_blinded = 0
   AND p.post_content LIKE '%런지%'
 ORDER BY p.post_created_at DESC;
+
+# 제목, 내용 둘다 포함된 키워드 검색
+SELECT p.post_id
+     , p.post_title
+     , p.post_content
+     , p.post_created_at
+     , p.post_updated_at
+     , p.user_id
+     , COUNT(c.comment_id) AS comment_count
+FROM community_post p
+         JOIN comment c ON c.user_id = p.user_id
+WHERE post_is_blinded = 0
+  AND c.comment_is_blinded = 0
+  AND (p.post_title LIKE '%운동%'
+    OR p.post_content LIKE '%운동%')
+GROUP BY p.post_id, p.post_title, p.post_content, p.post_created_at, p.post_updated_at, p.user_id
+ORDER BY p.post_created_at DESC;
