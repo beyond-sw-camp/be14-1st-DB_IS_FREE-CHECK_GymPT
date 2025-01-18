b3b298b3e0e74b48a049593c13e923e7b8b0c82c
MJay1123 <tjalswhd1@naver.com>
2025-01-18 15:17:36 +0900

    스튜디오 정보 수정 기능 추가

--- studio/studio_info_update.sql
+++ studio/studio_info_update.sql
@@ -1,13 +1,9 @@
--- 관리자 스튜디오 정보 등록 기능
-INSERT INTO studio (studio_id, studio_name, studio_address,
-                    studio_des, studio_price, created_at, updated_at)
-SELECT 100,
-    '포마토 스튜디오',
-    '서울시 동작구 어쩌구',
-    '이 스튜디오 앞에는 비둘기가 많이 삽니다..',
-    3500,
-    now(),
-    null
-WHERE NOT EXISTS (SELECT 1
-                  FROM studio
-                  WHERE studio_id = 100);
+-- 관리자 스튜디오 정보 수정 기능
+
+UPDATE studio
+	 SET studio_name = '포마토 스튜디오',
+	 	  studio_address = '서울특별시',
+	 	  studio_des = '테스트 설명',
+	 	  studio_price = 300000,
+	 	  updated_at = CURRENT_DATE
+  WHERE studio_id = 3;
