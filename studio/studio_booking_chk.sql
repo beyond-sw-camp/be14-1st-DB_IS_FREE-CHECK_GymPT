e7bb24ad8094b5fd1310bcf3e93dc8491dfd9eb3
Gombo2 <rhtjddus0502@gmail.com>
2025-01-19 21:58:45 +0900

    테케 이름 수정 및 예약 가능 여부 확인 조건 변경

--- studio/studio_booking_chk.sql
+++ studio/studio_booking_chk.sql
@@ -5,7 +5,7 @@ SELECT
             SELECT 1
             FROM studio_booking sb
             WHERE sb.studio_id = 1 -- 예약하려는 스튜디오 ID
-              AND (sb.start_time < '2025-02-07 16:00:00' AND sb.end_time > '2025-02-07 14:00:00') -- 겹치는 시간 검사
+              AND (sb.start_time <= '2025-02-07 16:00:00' AND sb.end_time >= '2025-02-07 14:00:00') -- 겹치는 시간 검사
         ) THEN '예약 불가'
         ELSE '예약 가능'
         END AS 예약상태;
